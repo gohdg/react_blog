01801b11287c5a43f34d07d405159415a7a7fd40
gohdg <gohdg@naver.com>
2020-08-24 22:41:29 +0900

    2nd commit

---
+++ src/components/Hero/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+import Card from '../UI/Card'
+
+function Hero() {
+    return (
+        <div>
+            <Card>
+                DG Hong
+            </Card>
+        </div>
+    )
+}
+
+export default Hero
